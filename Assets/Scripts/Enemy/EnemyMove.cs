49a3aa66ce5a5b3c7f2701e813212ef307ffe550
Fanera <egor.powwer@gmail.com>
2025-12-23 05:25:58 +1000

    движение врагов

--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -1,8 +1,13 @@
+using System.Collections;
+using NUnit.Framework;
 using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
     [SerializeField] public float speed;
+    public float distance = 0;
+    public float timer;
+    private bool isActive;
     private Transform playerTransform;
 
     void Start()
@@ -12,12 +17,21 @@ public class EnemyMove : MonoBehaviour
 
     void Update()
     {
-        Move();
+        if (MovementVector().magnitude < distance * 10 && !isActive)
+        {
+            isActive = true;
+        }
+
+        if (MovementVector().magnitude > distance && isActive)
+        {
+            StartCoroutine(Move());
+        }
     }
 
-    private void Move()
+    private IEnumerator Move()
     {
-        transform.position += speed * MovementVector() * Time.deltaTime;
+        transform.position += speed * MovementVector().normalized * Time.deltaTime;
+        yield return new WaitForSeconds(timer);
     }
 
     private Vector3 MovementVector()
